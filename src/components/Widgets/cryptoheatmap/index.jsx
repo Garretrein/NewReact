84ba0ce09de6be1a473d851f6a53503e619c1ba6
Garrett Reinhart <garretrein1984@gmail.com>
2023-12-25 17:37:51 -0800

    Ticker market Update

---
+++ src/components/Widgets/cryptoheatmap/index.jsx
@@ -0,0 +1,43 @@
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from "react";
+
+const CryptoHeatmap = () => {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-crypto-coins-heatmap.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+        {
+          "dataSource": "Crypto",
+          "blockSize": "market_cap_calc",
+          "blockColor": "change",
+          "locale": "en",
+          "symbolUrl": "",
+          "colorTheme": "light",
+          "hasTopBar": false,
+          "isDataSetEnabled": false,
+          "isZoomEnabled": true,
+          "hasSymbolTooltip": true,
+          "width": "100%",
+          "height": "100%"
+        }`;
+    container.current.appendChild(script);
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/" target="_blank" rel="noopener noreferrer">
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default CryptoHeatmap;
---
+++ src/components/marketmood/stock/index.jsx
@@ -0,0 +1,76 @@
+// TradingViewWidget.jsx
+import React, { useEffect, useRef, memo } from "react";
+
+const StockMood = () => {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+        {
+          "symbols": [
+            [
+              "NASDAQ:TSLA|1M"
+            ],
+            [
+              "NASDAQ:AAPL|1M"
+            ],
+            [
+              "NASDAQ:MSFT|1M"
+            ],
+            [
+              "NASDAQ:COIN|1M"
+            ]
+          ],
+          "chartOnly": true,
+          "width": "100%",
+          "height": "100%",
+          "locale": "en",
+          "colorTheme": "dark",
+          "autosize": true,
+          "showVolume": false,
+          "showMA": false,
+          "hideDateRanges": false,
+          "hideMarketStatus": false,
+          "hideSymbolLogo": false,
+          "scalePosition": "right",
+          "scaleMode": "Normal",
+          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+          "fontSize": "10",
+          "noTimeScale": false,
+          "valuesTracking": "1",
+          "changeMode": "price-and-percent",
+          "chartType": "area",
+          "maLineColor": "#2962FF",
+          "maLineWidth": 1,
+          "maLength": 9,
+          "lineWidth": 2,
+          "lineType": 0,
+          "dateRanges": [
+            "1d|1",
+            "1m|30",
+            "3m|60",
+            "12m|1D",
+            "60m|1W",
+            "all|1M"
+          ]
+        }`;
+    container.current.appendChild(script);
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/" rel="noopener noreferrer" target="_blank">
+          <span className="blue-text">StockMood</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default StockMood;
